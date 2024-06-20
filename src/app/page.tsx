871056b3acac84de479a8d41cfc23c6ecd7f2b5d
Mateus Sales <mateussales98@outlook.com>
2024-06-20 13:50:30 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,28 @@
-"use client"
-import Header from "../sections/header/Header";
-import AboutUs from "../sections/about-us/AboutUs";
-import Client from "../sections/client/Client";
-import Products from "../sections/product/Products";
-import Segments from "../sections/segments/Segments";
-import Differential from "../sections/differential/Diferential";
-import Feedback from "../sections/feedback/Feedback";
-import Questions from "../sections/questions/Questions";
-import Footer from "../sections/footer/Footer";
+'use client';
+import AboutUs from '../sections/about-us/AboutUs';
+import Client from '../sections/client/Client';
+import Differential from '../sections/differential/Diferential';
+import Feedback from '../sections/feedback/Feedback';
+import Footer from '../sections/footer/Footer';
+import Header from '../sections/header/Header';
+import Price from '../sections/price/Price';
+import Products from '../sections/product/Products';
+import Questions from '../sections/questions/Questions';
+import Segments from '../sections/segments/Segments';
 
 export default function Home() {
-  return (
-    <main className="flex flex-col p-0 mx-auto b-0 justify-center max-w-[1440px] items-center">
-      <Header/>
-      <Products/> 
-      <Client/>
-      <AboutUs/>
-      <Feedback/>
-      <Segments/>
-      <Differential/>
-      <Questions/>
-      <Footer/>
-    </main>
-  );
+	return (
+		<main className='flex flex-col p-0 mx-auto b-0 justify-center max-w-[1440px] items-center'>
+			<Header />
+			<Products />
+			<Price />
+			<Client />
+			<AboutUs />
+			<Feedback />
+			<Segments />
+			<Differential />
+			<Questions />
+			<Footer />
+		</main>
+	);
 }
