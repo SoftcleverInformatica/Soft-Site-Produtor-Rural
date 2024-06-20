3a2c2e45eb72ff63fffabf4fc69798b63db81912
Mateus Sales <mateussales98@outlook.com>
2024-06-20 15:01:35 -0300

    fix icons

--- src/sections/price/Price.tsx
+++ src/sections/price/Price.tsx
@@ -1,40 +1,51 @@
 import ButtonExpert from '@/src/components/button_expert';
+import {
+	Calculator,
+	CashRegister,
+	ChartBar,
+	File,
+	HandArrowDown,
+	HandArrowUp,
+	HandCoins,
+	Package,
+	Sparkle
+} from '@phosphor-icons/react';
 
 export default function Price() {
 	const planos = [
 		{
 			plan: 'Básico',
 			fatures: [
-				{ item: 'Emissão de até 20 notas mensais' },
-				{ item: 'Controle de Estoque' },
-				{ item: 'Cálculos automatizados' },
-				{ item: 'Relatórios Gerenciais.' },
+				{ item: 'Emissão de até 20 notas mensais', icon: <File weight='light' size={20} /> },
+				{ item: 'Controle de Estoque', icon: <Package weight='light' size={20} /> },
+				{ item: 'Cálculos automatizados', icon: <Calculator weight='light' size={20} /> },
+				{ item: 'Relatórios Gerenciais.', icon: <ChartBar weight='light' size={20} /> },
 			],
 			value: '89,90',
 		},
 		{
 			plan: 'Intermediário',
-      recommended: true,
+			recommended: true,
 			fatures: [
-				{ item: 'Emissão de até 40 notas mensais' },
-				{ item: 'Pedido de Venda.' },
-				{ item: 'Controle de Estoque' },
-				{ item: 'Cálculos automatizados' },
-				{ item: 'Relatórios Gerenciais.' },
+				{ item: 'Emissão de até 40 notas mensais', icon: <File weight='light' size={20} /> },
+				{ item: 'Pedido de Venda.', icon: <HandCoins weight='light' size={20} /> },
+				{ item: 'Controle de Estoque', icon: <Package weight='light' size={20} /> },
+				{ item: 'Cálculos automatizados', icon: <Calculator weight='light' size={20} /> },
+				{ item: 'Relatórios Gerenciais.', icon: <ChartBar weight='light' size={20} /> },
 			],
 			value: '139,90',
 		},
 		{
 			plan: 'Avançado',
 			fatures: [
-				{ item: 'Emissão de até 100 notas mensais' },
-				{ item: 'Pedido de Venda.' },
-				{ item: 'Pedido de Compra.' },
-				{ item: 'Financeiro a Pagar.' },
-				{ item: 'Financeiro a Receber.' },
-				{ item: 'Controle de Estoque' },
-				{ item: 'Cálculos automatizados' },
-				{ item: 'Relatórios Gerenciais.' },
+				{ item: 'Emissão de até 100 notas mensais', icon: <File weight='light' size={20} /> },
+				{ item: 'Pedido de Venda.', icon: <CashRegister weight='light' size={20} /> },
+				{ item: 'Pedido de Compra.', icon: <HandCoins weight='light' size={20} /> },
+				{ item: 'Financeiro a Pagar.', icon: <HandArrowUp weight='light' size={20} /> },
+				{ item: 'Financeiro a Receber.', icon: <HandArrowDown weight='light' size={20} /> },
+				{ item: 'Controle de Estoque', icon: <Package weight='light' size={20} /> },
+				{ item: 'Cálculos automatizados', icon: <Calculator weight='light' size={20} /> },
+				{ item: 'Relatórios Gerenciais.', icon: <ChartBar weight='light' size={20} /> },
 			],
 			value: '249,90',
 		},
@@ -43,6 +54,7 @@ export default function Price() {
 			fatures: [
 				{
 					item: 'Você monta o seu próprio pacote definindo a quantidade de notas que você necessita e os módulos que deseja utilizar',
+					icon: <Sparkle weight='light' color='#C2410C' size={70} />,
 				},
 			],
 		},
@@ -59,18 +71,18 @@ export default function Price() {
 							className={`flex flex-col gap-1 p-2 pt-2 rounded-md  ${_.recommended ? 'bg-sc' : ''} `}>
 							<span className='text-white font-bold h-6 pl-3'>{_.recommended ? 'Recomendado' : ''}</span>
 							<div
-								className={`flex flex-col flex-1 gap-4 justify-between px-4 py-8 bg-white rounded-md border hover:drop-shadow-[0px_0px_10px_rgba(36,173,181,.75)] border-[#003658]/50 ${_.recommended ? 'drop-shadow-[0px_0px_6px_rgba(36,173,181,1)]' : ''}  lg:px-8 lg:py-12 lg:gap-5 max-w-[18rem]`}>
+								className={`flex flex-col flex-1 gap-4 justify-between px-4 py-8 bg-white rounded-md border transition ease-in-out delay-150 hover:drop-shadow-[0px_0px_10px_rgba(36,173,181,.75)] border-[#003658]/50 ${_.recommended ? 'drop-shadow-[0px_0px_6px_rgba(36,173,181,1)]' : ''}  lg:px-8 lg:py-12 lg:gap-5 max-w-[18rem]`}>
 								<div>
 									<h4 className='text-sc font-semibold text-2xl'>{_.plan}</h4>
 									{/* <p className='text-[#003658]/70 '>Description</p> */}
 								</div>
-								<ul className='flex flex-col gap-1'>
+								<ul className='flex flex-col gap-2'>
 									{_.fatures.map((_, i) => {
 										return (
 											<li
 												key={i}
-												className='text-[#003658]/90 '>
-												<span>{_.item}</span>
+												className='text-[#003658]/90 text-xs flex items-start gap-2'>
+												{_.icon}<span>{_.item}</span>
 											</li>
 										);
 									})}
